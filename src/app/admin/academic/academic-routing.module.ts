d72becc31cce3e049375b552e13c40952f5a0e28
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2021-02-28 13:56:02 +0530

    Initial Commit 1

---
+++ src/app/admin/academic/academic-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AcademicComponent } from './academic/academic.component';
+
+const routes: Routes = [
+  {
+    path: 'academic', 
+    component: AcademicComponent,
+    children: [
+      {
+        path: 'mastersettings', 
+        loadChildren: () => import('./mastersettings/mastersettings.module').then(m => m.MastersettingsModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AcademicRoutingModule { }
